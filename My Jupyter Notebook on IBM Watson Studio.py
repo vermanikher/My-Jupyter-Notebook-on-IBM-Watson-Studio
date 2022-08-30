8f7b1e78066564175b8a53c6a215565267934393
vermanikher <43517425+vermanikher@users.noreply.github.com>
2022-08-30 16:10:24 +0530

    Add files via upload
    This file was created and downloaded on IBM Watson platform and is being created to complete an assignment.

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Nikher Verma**
+# Data Scientist 
+# 
+
+# *I am interested in data science because I am good at playing with numbers and at the same time I am having interest in story telling*
+# 
+
+# #### In the next cell I am going to print "Hello World"
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# Header 1 | Header2 
+#  1            2
+# ---          ---
+
+# - This is how you use bullets
+# * this also 
+#     * sub-bullets 
+
+# 1. if you want numbering -
+#     1. Sub-numbering 
+
+# > Using a blocked quotes
+
+# Adding a hyper link 
+# [click here to google](https://www.google.com/)
+
+# In[ ]:
+
+
+
+
